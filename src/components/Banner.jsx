fc8ecf77a1bcab19125e0ee0a1a33042d183e76b
sharif57 <sharifmahamud577951@gmail.com>
2024-08-20 20:20:50 +0600

    featues secation done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,9 @@
 const Banner = () => {
-    return <div className="lg:flex ">
-        <div>
+    return <div className="lg:flex container mx-auto gap-20">
+        <div className="w-1/2">
             <img src="https://i.ibb.co/mCdQsF0/Container-1.png" alt="" />
         </div>
-        <div>
+        <div className="w-1/2">
             <img src="https://i.ibb.co/m8tg72Y/Container.png" alt="" />
         </div>
     </div>;
--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,55 +1,88 @@
 const Blog = () => {
-    return <div className="bg-[#141414]">
-        <div className="container mx-auto lg:flex gap-10 items-center justify-center pt-[150px] pb-[150px]">
-            <div className="space-y-6">
-                <img src="https://i.ibb.co/ggQDHhb/Icon.png" alt="" />
-                <h1 className="text-4xl text-white font-semibold mb-4">Future Technology Blog</h1>
-                <p >Stay informed with our blog section dedicated to future technology.</p>
-            </div>
-            <div className=" grid gap-4 rounded-lg grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
-                <div className="bg-[#191919] p-6 rounded-lg border-dashed border">
-                    <h1 className="text-3xl font-semibold text-white">Quantity</h1>
-                    <p  className="pt-3">Over 1,000 articles on emerging tech trends and breakthroughs.</p>
+    return (
+        <div className="bg-[#141414]">
+            <div className="container mx-auto">
+                {/* First Section */}
+                <div className="lg:flex gap-10 items-center justify-center pt-[100px] ">
+                    <div className="space-y-6">
+                        <img src="https://i.ibb.co/ggQDHhb/Icon.png" alt="" />
+                        <h1 className="text-4xl text-white font-semibold mb-4">Future Technology Blog</h1>
+                        <p>Stay informed with our blog section dedicated to future technology.</p>
+                    </div>
+                    <div className="grid gap-4 rounded-lg grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+                        <div className="bg-[#191919] p-6 rounded-lg border-dashed border">
+                            <h1 className="text-3xl font-semibold text-white">Quantity</h1>
+                            <p className="pt-3">Over 1,000 articles on emerging tech trends and breakthroughs.</p>
+                        </div>
+                        <div className="bg-[#191919] p-6 rounded-lg border-dashed border">
+                            <h1 className="text-3xl font-semibold text-white">Variety</h1>
+                            <p className="pt-3">Articles cover fields like AI, robotics, biotechnology, and more.</p>
+                        </div>
+                        <div className="bg-[#191919] p-6 rounded-lg border-dashed border">
+                            <h1 className="text-3xl font-semibold text-white">Frequency</h1>
+                            <p className="pt-3">Fresh content added daily to keep you up to date.</p>
+                        </div>
+                        <div className="bg-[#191919] p-6 rounded-lg border-dashed border">
+                            <h1 className="text-3xl font-semibold text-white">Authoritative</h1>
+                            <p className="pt-3">Written by our team of tech experts and industry professionals.</p>
+                        </div>
+                    </div>
                 </div>
-                <div className="bg-[#191919] p-6 rounded-lg border-dashed border ">
-                    <h1 className="text-3xl font-semibold text-white">Variety</h1>
-                    <p className="pt-3">Articles cover fields like AI, robotics, biotechnology, and more.</p>
+                <div className="lg:flex gap-10 items-center justify-center pt-[100px] ">
+                    <div className="space-y-6">
+                        <img src="https://i.ibb.co/DVTX1TY/Icon-1.png" alt="" />
+                        <h1 className="text-4xl text-white font-semibold mb-4">Research Insights Blogs</h1>
+                        <p>Stay informed with our blog section dedicated to future technology.</p>
+                    </div>
+                    <div className="grid gap-4 rounded-lg grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+                        <div className="bg-[#191919] p-6 rounded-lg border-dashed border">
+                            <h1 className="text-3xl font-semibold text-white">Depth</h1>
+                            <p className="pt-3">Over 1,000 articles on emerging tech trends and breakthroughs.</p>
+                        </div>
+                        <div className="bg-[#191919] p-6 rounded-lg border-dashed border">
+                            <h1 className="text-3xl font-semibold text-white">Graphics</h1>
+                            <p className="pt-3">Articles cover fields like AI, robotics, biotechnology, and more.</p>
+                        </div>
+                        <div className="bg-[#191919] p-6 rounded-lg border-dashed border">
+                            <h1 className="text-3xl font-semibold text-white">Trends</h1>
+                            <p className="pt-3">Fresh content added daily to keep you up to date.</p>
+                        </div>
+                        <div className="bg-[#191919] p-6 rounded-lg border-dashed border">
+                            <h1 className="text-3xl font-semibold text-white">Contributors</h1>
+                            <p className="pt-3">Written by our team of tech experts and industry professionals.</p>
+                        </div>
+                    </div>
                 </div>
-                <div className="bg-[#191919] p-6 rounded-lg border-dashed border">
-                    <h1 className="text-3xl font-semibold text-white">Frequency</h1>
-                    <p className="pt-3">Fresh content added daily to keep you up to date.</p>
-                </div>
-                <div className="bg-[#191919] p-6 rounded-lg border-dashed border">
-                    <h1 className="text-3xl font-semibold text-white">Authoritative</h1>
-                    <p className="pt-3">Written by our team of tech experts and industry professionals.</p>
-                </div>
+
+                {/* Second Section */}
+                {/* <div className="lg:flex gap-10 items-center justify-center pt-[60px] ">
+                    <div className="space-y-6">
+                        <img src="https://i.ibb.co/DVTX1TY/Icon-1.png" alt="" />
+                        <h1 className="text-4xl text-white font-semibold mb-4">Research Insights Blogs</h1>
+                        <p>Dive deep into future technology concepts with our research section.</p>
+                    </div>
+                    <div className="grid gap-4 rounded-lg grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+                        <div className="bg-[#191919] p-6 rounded-lg border-dashed border">
+                            <h1 className="text-3xl font-semibold text-white">Depth</h1>
+                            <p className="pt-3">500+ research articles for in-depth understanding.</p>
+                        </div>
+                        <div className="bg-[#191919] p-6 rounded-lg border-dashed border">
+                            <h1 className="text-3xl font-semibold text-white">Graphics</h1>
+                            <p className="pt-3">Visual aids and infographics to enhance comprehension.</p>
+                        </div>
+                        <div className="bg-[#191919] p-6 rounded-lg border-dashed border">
+                            <h1 className="text-3xl font-semibold text-white">Trends</h1>
+                            <p className="pt-3">Explore emerging trends in future technology research.</p>
+                        </div>
+                        <div className="bg-[#191919] p-6 rounded-lg border-dashed border">
+                            <h1 className="text-3xl font-semibold text-white">Contributors</h1>
+                            <p className="pt-3">Contributions from tech researchers and academics.</p>
+                        </div>
+                    </div>
+                </div> */}
             </div>
         </div>
-        <div className="container mx-auto lg:flex gap-10 items-center justify-center pt-[100px] pb-[150px]">
-            <div className="space-y-6">
-                <img src="https://i.ibb.co/ggQDHhb/Icon.png" alt="" />
-                <h1 className="text-4xl text-white font-semibold mb-4">Future Technology Blog</h1>
-                <p >Stay informed with our blog section dedicated to future technology.</p>
-            </div>
-            <div className=" grid gap-4 rounded-lg grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
-                <div className="bg-[#191919] p-6 rounded-lg border-dashed border">
-                    <h1 className="text-3xl font-semibold text-white">Quantity</h1>
-                    <p  className="pt-3">Over 1,000 articles on emerging tech trends and breakthroughs.</p>
-                </div>
-                <div className="bg-[#191919] p-6 rounded-lg border-dashed border ">
-                    <h1 className="text-3xl font-semibold text-white">Variety</h1>
-                    <p className="pt-3">Articles cover fields like AI, robotics, biotechnology, and more.</p>
-                </div>
-                <div className="bg-[#191919] p-6 rounded-lg border-dashed border">
-                    <h1 className="text-3xl font-semibold text-white">Frequency</h1>
-                    <p className="pt-3">Fresh content added daily to keep you up to date.</p>
-                </div>
-                <div className="bg-[#191919] p-6 rounded-lg border-dashed border">
-                    <h1 className="text-3xl font-semibold text-white">Authoritative</h1>
-                    <p className="pt-3">Written by our team of tech experts and industry professionals.</p>
-                </div>
-            </div>
-        </div>
-    </div>;
+    );
 };
+
 export default Blog;

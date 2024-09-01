8bbdd67702fed149b1b0d53e438966f3553a3401
sharif57 <sharifmahamud577951@gmail.com>
2024-09-01 21:56:26 +0600

    data map

--- src/Dashboard/UserManage.jsx
+++ src/Dashboard/UserManage.jsx
@@ -1,7 +1,46 @@
 import { useEffect, useState } from "react";
+import { MdAdminPanelSettings } from "react-icons/md";
+import Swal from "sweetalert2";
 
 const UserManage = () => {
     const [ManageUsers, setManageUsers] = useState([]);
+
+    const handleDelete = _id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        })
+            .then((result) => {
+                if (result.isConfirmed) {
+                    fetch(`http://localhost:5000/userDelete/${_id}`, {
+                        method: 'DELETE'
+                    })
+                        .then(res => res.json())
+                        .then(data => {
+                            if (data.deletedCount > 0) {
+
+                                Swal.fire({
+                                    title: "Deleted!",
+                                    text: "Your item has been deleted.",
+                                    icon: "success"
+                                });
+
+                                const remaining = ManageUsers.filter(i => i._id !== _id);
+                                setManageUsers(remaining)
+                                // console.log('delete');
+                                // setSort(remaining)
+                            }
+                        })
+                }
+            })
+
+    }
+
 
     useEffect(() => {
         fetch('http://localhost:5000/users')
@@ -68,12 +107,7 @@ const UserManage = () => {
                                             >
                                                 Name
                                             </th>
-                                            <th
-                                                scope="col"
-                                                className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500"
-                                            >
-                                                Age
-                                            </th>
+
                                             <th
                                                 scope="col"
                                                 className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500"
@@ -115,9 +149,7 @@ const UserManage = () => {
                                                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">
                                                     {user.name}
                                                 </td>
-                                                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">
-                                                    {user.age}
-                                                </td>
+
                                                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">
                                                     {user.email}
                                                 </td>
@@ -126,16 +158,34 @@ const UserManage = () => {
                                                         type="button"
                                                         className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 focus:outline-none focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:text-blue-400"
                                                     >
-                                                        Admin
+                                                        <MdAdminPanelSettings className="size-11" />
                                                     </button>
                                                 </td>
                                                 <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+
                                                     <button
-                                                        type="button"
-                                                        className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 focus:outline-none focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:text-blue-400"
+                                                        onClick={() => handleDelete(user._id)}
+                                                        className="flex justify-center items-center gap-2 w-28 h-12 cursor-pointer rounded-md shadow-2xl text-white font-semibold bg-gradient-to-r from-[#fb7185] via-[#e11d48] to-[#be123c] hover:shadow-xl hover:shadow-red-500 hover:scale-105 duration-300 hover:from-[#be123c] hover:to-[#fb7185]"
                                                     >
+                                                        <svg viewBox="0 0 15 15" className="w-5 fill-white">
+                                                            <svg
+                                                                className="w-6 h-6"
+                                                                stroke="currentColor"
+                                                                strokeWidth="1.5"
+                                                                viewBox="0 0 24 24"
+                                                                fill="none"
+                                                                xmlns="http://www.w3.org/2000/svg"
+                                                            >
+                                                                <path
+                                                                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                                                                    strokeLinejoin="round"
+                                                                    strokeLinecap="round"
+                                                                ></path>
+                                                            </svg>
+                                                        </svg>
                                                         Delete
                                                     </button>
+
                                                 </td>
                                             </tr>
                                         ))}
